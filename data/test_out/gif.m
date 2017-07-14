249153494ae83b05b7349edf251540692ffc82f2
Yijun Li <liyjcse@gmail.com>
2017-07-14 16:48:27 -0700

    Add files via upload

---
+++ data/test_out/gif.m
@@ -0,0 +1,11 @@
+outputVideo = VideoWriter('002937.avi');
+outputVideo.FrameRate =24;
+open(outputVideo)
+  
+for i = 1:500 
+   sname = strcat(num2str(i),'.jpg');
+   img=imread(strcat('./', sname));
+   writeVideo(outputVideo,uint8(img));
+end
+
+close(outputVideo)
